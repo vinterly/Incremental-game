58fb7c51eb968b37d7d394038a21a649a6d0b44c
vinterly <mariee.h@outlook.com>
2025-04-28 21:50:00 +0200

    add house button, button as component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<button class="add-gold-btn" type="button">{@render children()}</button>
+
+<style>
+	.add-gold-btn {
+		border: none;
+		outline: none;
+		cursor: pointer;
+		background-color: #6c5ce7;
+		padding: 0.6rem 1.3rem;
+		font-size: 0.8rem;
+		font-weight: 700;
+		color: #fff;
+		border-radius: 0.3rem;
+		transition: all ease 0.1s;
+		box-shadow: 0 5px 0 #a29bfe;
+	}
+
+	.add-gold-btn:active {
+		transform: translateY(5px);
+		box-shadow: 0 0 0 0 #a29bfe;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import HousePlus from "@lucide/svelte/icons/house-plus";
+	import Button from "$lib/components/Button.svelte";
 	import "../app.css";
 
 	let gold = $state(0);
@@ -11,7 +13,10 @@
 <main>
 	<h1>A single house stands in the clearing</h1>
 	<p class="gold-counter">{gold}</p>
-	<button class="add-gold-btn" type="button" onclick={addGold}>+1</button>
+	<div class="buttons-row">
+		<Button onclick={addGold}>+1</Button>
+		<Button><HousePlus /></Button>
+	</div>
 </main>
 
 <style>
@@ -33,22 +38,8 @@
 		margin-bottom: 1rem;
 	}
 
-	.add-gold-btn {
-		border: none;
-		outline: none;
-		cursor: pointer;
-		background-color: #6c5ce7;
-		padding: 0.6rem 1.3rem;
-		font-size: 0.8rem;
-		font-weight: 700;
-		color: #fff;
-		border-radius: 0.3rem;
-		transition: all ease 0.1s;
-		box-shadow: 0 5px 0 #a29bfe;
-	}
-
-	button:active {
-		transform: translateY(5px);
-		box-shadow: 0 0 0 0 #a29bfe;
+	.buttons-row {
+		display: flex;
+		gap: 1rem;
 	}
 </style>
